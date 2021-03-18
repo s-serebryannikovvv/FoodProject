bcee85adefb3d15d52d2e4cec4ed8fe244f9dbf3
Sergey <s.serebryannikovvv@mail.ru>
2021-03-18 20:57:24 +0300

    addTimer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+	// Tabs
+
 	const tabs = document.querySelectorAll('.tabheader__item'),
 		tabsContent = document.querySelectorAll('.tabcontent'),
 		tabsParent = document.querySelector('.tabheader__items');
@@ -36,10 +38,6 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 
-
-
-
-
 	// tabsParent.addEventListener('click', (event) => { //навешиваем отслеживание события на родительский элемент
 	// 	const target = event.target; // присваиваем в переменную значение куда был клик
 	// 	if (target && target.classList.contains('tabheader__item')) { // проверяем есть ли у события target и есть ли класс 
@@ -52,5 +50,60 @@ document.addEventListener('DOMContentLoaded', () => {
 	// 	}
 	// });
 
+	//Timer 
+
+	const deadline = '2021-03-19'; //Дата дедлайна
+
+	function getTimeRemaining(endtime) {
+		const t = Date.parse(endtime) - Date.parse(new Date()), //парсим строчное значение даты в миллисекунды и вычитаем из даты в данный момент времени (получаем разницу)
+			days = Math.floor(t / (1000 * 60 * 60 * 24)), //делим миллисек. до дедлайна на  миллисекунды в сутках
+			hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+			minutes = Math.floor((t / 1000 / 60) % 60),
+			seconds = Math.floor((t / 1000) % 60);
+
+		return { //возвращаем объект
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	}
+
+	function getZero(num) {
+		if (num >= 0 && num < 10) {
+			return `0${num}`;
+		} else {
+			return num;
+		}
+	}
+
+	function setClock(selector, endtime) {
+		const timer = document.querySelector(selector),
+			days = timer.querySelector('#days'),
+			hours = timer.querySelector('#hours'),
+			minutes = timer.querySelector('#minutes'),
+			seconds = timer.querySelector('#seconds'),
+			timeInterval = setInterval(updateClock, 1000);
+
+		updateClock();
+
+		function updateClock() {
+			const t = getTimeRemaining(endtime);
+
+			days.innerHTML = getZero(t.days);
+			hours.innerHTML = getZero(t.hours);
+			minutes.innerHTML = getZero(t.minutes);
+			seconds.innerHTML = getZero(t.seconds);
+
+
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
+			}
+		}
+
+	}
+
+	setClock('.timer', deadline);
 
 });
